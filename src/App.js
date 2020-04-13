b41251b271da4ae2f978d28f8f1b3cafeddfedc9
pedram <pedram.post@gmail.com>
2020-04-13 12:01:07 -0700

    to the point that I add Url to every Item in the directory

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homePage.component'
 import './App.css';
 
+const Hats = (props) => {
+  console.log(props);
+  return <h2>Hello Hats</h2>;
+}
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={Hats} />
+    </Switch>
+  </div>;
 }
 
 export default App;
